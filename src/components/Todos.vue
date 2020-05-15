93ceac86210e536181266da8b072cb0725a83615
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-15 16:15:06 -0700

    passing props  and rendering list of records

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,19 @@
 <template>
     <div>
         <div v-for="todo in todos" v-bind:key="todo.id">
-            <h3>Another Todo</h3>
+            <TodoItem v-bind:todo="todo"/> 
         </div>
     </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem';
+
 export default {
     name: "Todos",
+    components: {
+        TodoItem
+    },
     props: ["todos"]
 }
 </script>
